bd5d683b2ca67b22f1d29f999b44c86afca9e136
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-07 11:35:52 -0500

    Input Validation (Data-Driven)

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'data-driven',
-    templateUrl: 'data-driven.component.html'
+    templateUrl: 'data-driven.component.html',
+    styles: [`
+    input.ng-invalid {
+      border: 1px solid red;
+    }
+  `]
 })
 export class DataDrivenComponent {
     myForm: FormGroup;
 
     constructor() {
         this.myForm = new FormGroup({
-            'username': new FormControl(),
-            'email': new FormControl(),
-            'password': new FormControl()
+            'username': new FormControl('Dmitry', Validators.required),
+            'email': new FormControl('dmitry@test.com', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+            'password': new FormControl('', Validators.required)
         });
     }
 
-    onSubmit(){
+    onSubmit() {
         console.log(this.myForm.value);
     }
 }
