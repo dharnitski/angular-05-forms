15b9737d4b07c032fd3846f60ea2368f275cd77e
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-07 11:52:44 -0500

    Radio Buttons (Data-Driven)

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -13,13 +13,19 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class DataDrivenComponent {
     myForm: FormGroup;
 
+    genders = [
+        'male',
+        'female'
+    ];
+
     constructor() {
         this.myForm = new FormGroup({
             'userData': new FormGroup({
                 'username': new FormControl('Dmitry', Validators.required),
                 'email': new FormControl('dmitry@test.com', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
             }),
-            'password': new FormControl('', Validators.required)
+            'password': new FormControl('', Validators.required),
+            'gender': new FormControl('female')
         });
     }
 
