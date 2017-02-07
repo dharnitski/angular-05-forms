b7399eb838bbc2add69e4026c1f0bc146c38d9aa
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-07 11:48:51 -0500

    Form Groups (Data-Driven)

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -15,8 +15,10 @@ export class DataDrivenComponent {
 
     constructor() {
         this.myForm = new FormGroup({
-            'username': new FormControl('Dmitry', Validators.required),
-            'email': new FormControl('dmitry@test.com', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+            'userData': new FormGroup({
+                'username': new FormControl('Dmitry', Validators.required),
+                'email': new FormControl('dmitry@test.com', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+            }),
             'password': new FormControl('', Validators.required)
         });
     }
